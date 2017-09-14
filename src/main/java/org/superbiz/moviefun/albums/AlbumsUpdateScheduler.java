508e0c0b6d95d1cfe4b48ad27d3ecd63331c7e79
Kannan <103209@AMB00687.local>
2017-09-14 12:48:25 +0530

    Background job with DB.

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
@@ -2,10 +2,14 @@ package org.superbiz.moviefun.albums;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAsync
@@ -18,8 +22,13 @@ public class AlbumsUpdateScheduler {
     private final AlbumsUpdater albumsUpdater;
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater) {
+    private DataSource ds;
+    private JdbcTemplate template;
+
+    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater, DataSource ds) {
+        this.ds = ds;
         this.albumsUpdater = albumsUpdater;
+        template = new JdbcTemplate(ds);
     }
 
 
@@ -27,12 +36,29 @@ public class AlbumsUpdateScheduler {
     public void run() {
         try {
             logger.debug("Starting albums update");
-            albumsUpdater.update();
-
-            logger.debug("Finished albums update");
+            if(startAlbumSchedulerTask()) {
+                logger.info("Start of album update...");
+                albumsUpdater.update();
+                logger.info("Album update complete.");
+            }
+            else {
+                logger.info("Another update running. Skipping...");
+            }
 
         } catch (Throwable e) {
             logger.error("Error while updating albums", e);
         }
     }
+
+    private boolean startAlbumSchedulerTask() {
+        int updatedRows = template.update(
+                "UPDATE album_scheduler_task" +
+                        " SET started_at = now()" +
+                        " WHERE started_at IS NULL" +
+                        " OR started_at < date_sub(now(), INTERVAL 2 MINUTE)"
+        );
+
+        return updatedRows > 0;
+    }
+
 }

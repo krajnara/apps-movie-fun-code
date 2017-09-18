0d52b85deb279ff137abcca2fd7a2fda68c6a214
Kannan <103209@AMB00687.local>
2017-09-18 15:12:46 +0530

    Split into components and applications.

--- components/movies/src/main/java/org/superbiz/moviefun/MoviesServlet.java
+++ components/movies/src/main/java/org/superbiz/moviefun/MoviesServlet.java
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Component;
 import org.superbiz.moviefun.movies.Movie;
 import org.superbiz.moviefun.movies.MoviesBean;
 
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -33,15 +32,17 @@ import java.util.List;
  * @version $Revision$ $Date$
  */
 @Component
-public class ActionServlet extends HttpServlet {
+public class MoviesServlet extends HttpServlet {
 
     private static final long serialVersionUID = -5832176047021911038L;
 
     public static int PAGE_SIZE = 5;
 
-    @EJB
     private MoviesBean moviesBean;
 
+    public MoviesServlet(MoviesBean bean) {
+        this.moviesBean = bean;
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request, response);
